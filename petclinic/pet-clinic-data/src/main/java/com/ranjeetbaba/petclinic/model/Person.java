cb3445f9bc922bef888b3bf7e61514af546e1e0a
Ranjeet Kumar Mishra <ranjeetkmishra95@gmail.com>
2018-12-25 00:41:32 +0530

    created services and BaseEntity

--- petclinic/pet-clinic-data/src/main/java/com/ranjeetbaba/petclinic/model/Person.java
+++ petclinic/pet-clinic-data/src/main/java/com/ranjeetbaba/petclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.ranjeetbaba.petclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
     private String firstName;
     private String lastName;
 
---
+++ petclinic/pet-clinic-data/src/main/java/com/ranjeetbaba/petclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package com.ranjeetbaba.petclinic.services;
+
+import com.ranjeetbaba.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
